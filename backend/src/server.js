c163c459ef3e7f7d54490a09ff493dcc3fe78eb9
Anshuman Sinha <115091319+anshuman0904@users.noreply.github.com>
2025-08-10 14:16:43 +0000

    MongoDB Setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 5001;
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-    console.log("Server started at port 5001");
+app.listen(port, () => {
+    console.log("Server started at port: ", port);
 });
