7dc19e43194483a211879562cb101dbd09945848
Anshuman Sinha <115091319+anshuman0904@users.noreply.github.com>
2025-08-15 11:27:15 +0000

    added functionality for rate limiting, using upstash

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ connectDB();
 
 //middleware
 app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
