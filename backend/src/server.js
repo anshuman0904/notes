5a97ebefbfb285c24794f7460838c0b33f03add7
Anshuman Sinha <115091319+anshuman0904@users.noreply.github.com>
2025-08-17 14:36:42 +0000

    Created home page

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
@@ -10,6 +12,9 @@ const app = express();
 const port = process.env.PORT || 5001;
 
 //middleware
+app.use(cors({
+    origin: "https://verbose-meme-rq94776g46g255q-5173.app.github.dev"
+}));
 app.use(express.json());
 app.use(rateLimiter);
 
