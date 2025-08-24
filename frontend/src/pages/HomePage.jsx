65b1900e45a00383fb19d96c6c5dcf904951a18e
Anshuman Sinha <115091319+anshuman0904@users.noreply.github.com>
2025-08-24 03:38:42 +0000

    added delete functionality

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
 import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
 import toast from "react-hot-toast"
 import api from "../lib/axios";
 
@@ -41,12 +42,14 @@ const HomePage = () => {
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
         
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map(note => (
               <div>
-                <NoteCard key={note._id} note={note} />
+                <NoteCard key={note._id} note={note} setNotes={setNotes} />
               </div>
             ))}
           </div>
