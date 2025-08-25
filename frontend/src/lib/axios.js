2de6e5e71505a8459ce866bf392c7389c6145480
Anshuman Sinha <anshuman@engineer.com>
2025-08-25 13:19:05 +0530

    prepared for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
-    baseURL : "http://127.0.0.1:5001/api"
+    baseURL : BASE_URL
 });
 
 export default api;
